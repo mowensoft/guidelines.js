f167cb835e225aa2af03416a5df43115413088ea
backwardsbowler <david@teapotcoder.com>
2015-06-22 12:51:35 -0500

    Initial guidelines for review

---
+++ guidelines.js
@@ -0,0 +1,28 @@
+// Coding Guidelines for Javacript
+
+var required = [
+	"Verify project runs",
+	"Verify unit tests pass if available"
+]
+
+var suggestted = [
+	"Minimal inline javascript",
+	"Runs on corporate standard browser",
+	"DRY",
+	"Minimal logic in controllers for Angular.js",
+	"Don't use uncommon abbreviations or acronyms",
+	"Minimal hard-coding/literals",
+	"Use promises instead of call-backs",
+	"Guidelines - This neat little library",
+	"Module/Class names are PascalCase",
+	"Function/variables shoudl be camelCase",
+	"Avoid $ in names",
+	"Avoid using underscore. They don't create private variables",
+	"When using comments make them useful, (DRY), and use JSDoc for methods",
+	"Consistent indentation",
+	"Braces should go on same line",
+]
+
+var future = [
+	"Run through linter"
+]
